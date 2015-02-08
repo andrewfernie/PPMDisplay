9d8d6c7a276c958439eefd6446811c98e9871dd6
Andrew Fernie <andrewjfernie@gmail.com>
2015-02-08 09:03:00 -0500

    updated project to VS2013

--- PPMDisplay/Visual Micro/.PPMDisplay.vsarduino.h
+++ PPMDisplay/Visual Micro/.PPMDisplay.vsarduino.h
@@ -1,14 +1,26 @@
+/* 
+	Editor: http://www.visualmicro.com
+	        visual micro and the arduino ide ignore this code during compilation. this code is automatically maintained by visualmicro, manual changes to this file will be overwritten
+	        the contents of the Visual Micro sketch sub folder can be deleted prior to publishing a project
+	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
+	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
+	
+	Hardware: Arduino Duemilanove w/ ATmega328, Platform=avr, Package=arduino
+*/
+
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-//Board = Arduino Duemilanove w/ ATmega328
 #define __AVR_ATmega328p__
 #define __AVR_ATmega328P__
-#define ARDUINO 101
+#define ARDUINO 105
 #define ARDUINO_MAIN
 #define __AVR__
 #define __avr__
 #define F_CPU 16000000L
 #define __cplusplus
+#define GCC_VERSION 40301
+#define ARDUINO_ARCH_AVR
+#define ARDUINO_AVR_ATMEGA328
 #define __inline__
 #define __asm__(x)
 #define __extension__
@@ -18,30 +30,35 @@
 #define __asm__ 
 #define __volatile__
 
-#define __builtin_va_list
+typedef void *__builtin_va_list;
 #define __builtin_va_start
 #define __builtin_va_end
-#define __DOXYGEN__
+//#define __DOXYGEN__
 #define __attribute__(x)
 #define NOINLINE __attribute__((noinline))
 #define prog_void
 #define PGM_VOID_P int
+
+#define NEW_H
             
 typedef unsigned char byte;
 extern "C" void __cxa_pure_virtual() {;}
-
-//
-//
-void read_ppm();
-void reset_display();
-void LCDSetPrintPos(uint8_t x, uint8_t y);
-void LCDPrint(const char v[]);
-void LCDClearScreen();
-void EnablePPMInterrupt();
-void DisablePPMInterrupt();
-
-#include "C:\Arduino-1.0.5\hardware\arduino\cores\arduino\arduino.h"
-#include "C:\Arduino-1.0.5\hardware\arduino\variants\standard\pins_arduino.h" 
-#include "C:\Users\andrew\Documents\Arduino\ArduinoProjectRepositories\Local_PPMDisplay\PPMDisplay\PPMDisplay.ino"
-#include "C:\Users\andrew\Documents\Arduino\ArduinoProjectRepositories\Local_PPMDisplay\PPMDisplay\PPMDisplay.h"
+#include <arduino.h>
+#include <pins_arduino.h> 
+#undef F
+#define F(string_literal) ((const PROGMEM char *)(string_literal))
+#undef cli
+#define cli()
+#define pgm_read_byte(address_short)
+#define pgm_read_word(address_short)
+#define pgm_read_word2(address_short)
+#define digitalPinToPort(P)
+#define digitalPinToBitMask(P) 
+#define digitalPinToTimer(P)
+#define analogInPinToBit(P)
+#define portOutputRegister(P)
+#define portInputRegister(P)
+#define portModeRegister(P)
+#include <PPMDisplay.ino>
+#include <PPMDisplay.h>
 #endif
